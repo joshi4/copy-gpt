5e700659b94bcd491d505e0d34443860e48c62b1
Shantanu <joshishantanu4@gmail.com>
2023-04-14 15:02:45 -0700

    ignore p tags to make copying text more robust

--- copy.js
+++ copy.js
@@ -1,5 +1,15 @@
 // add a copy button to each chatGPT response 
 
+function removeDuplicates(arr) {
+  let unique = [];
+  arr.forEach(element => {
+    if (!unique.includes(element)) {
+      console.log("pushing:", element);
+      unique.push(element);
+    }
+  });
+  return unique;
+}
 // Define the callback function to be executed when changes are detected
 const callback = function (mutationList, observer) {
   // Select all response elements on the page
@@ -25,14 +35,17 @@ const callback = function (mutationList, observer) {
     // Taken from the icon GPT uses for copying code
     button.innerHTML = '<svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" class="h-4 w-4" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"></path><rect x="8" y="2" width="8" height="4" rx="1" ry="1"></rect></svg>';
     button.addEventListener('click', () => {
-      const elements = box.querySelectorAll("*");
+      // Get all elements in a the response box
+      // sometimes when the response is large it is broken up into multiple p tags
+      // we already get the whole response when we get innerText of the box so we don't need to get the text of the p tags
+      const elements = box.querySelectorAll("* :not(p)");
       const textArray = [];
       for (let i = 0; i < elements.length; i++) {
         if (elements[i].innerText) {
           textArray.push(elements[i].innerText);
         }
       }
-      const uniqueTextArray = [...new Set(textArray)];
+      const uniqueTextArray = removeDuplicates(textArray);
       const text = uniqueTextArray.join('\n');
       navigator.clipboard.writeText(text).then(() => {
         console.log('Text copied to clipboard');
